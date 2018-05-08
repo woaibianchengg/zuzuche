d742ecfaa0719d95e7e0031b3e9031f7e6004c57
sakism <3275283525@qq.com>
2018-05-09 01:38:24 +0800

    更新横向滚动
    更新横向滚动

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,6 @@
 var roo = new Swiper('.swiper-container',{
 	slidesPerView:"auto",
 	centeredSlides:true,
-	spaceBetween:15,
 //	watchSlidesProgress:!0,
 //	slidesOffsetBefore:37,
 //	spaceBetween:17,
