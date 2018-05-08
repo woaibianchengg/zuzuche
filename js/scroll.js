3781f195edebced9dfcb8d711eb7c4445fbef1df
sakism <3275283525@qq.com>
2018-05-08 17:20:51 +0800

    Initial commit

---
+++ js/scroll.js
@@ -0,0 +1,8 @@
+var roo = new Swiper('.swiper-container',{
+	slidesPerView:"auto",
+	centeredSlides:true,
+	spaceBetween:15,
+//	watchSlidesProgress:!0,
+//	slidesOffsetBefore:37,
+//	spaceBetween:17,
+})
